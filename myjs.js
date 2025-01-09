7b764b0c2a21d603496a4b176b7a87a0d06788c9
73Noa <167832828+73Noa@users.noreply.github.com>
2025-01-09 15:07:42 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,1 +1,12 @@
-
+function keisan1(){var a = Number(document.getElementById("input1").value);
+                   var b = Number(document.getElementById("input2").value);
+                   alert( a + b );}
+function keisan2(){var c = Number(document.getElementById("input3").value);
+                   var d = Number(document.getElementById("input4").value);
+                   alert( c - d );}
+function keisan3(){var e = Number(document.getElementById("input5").value);
+                   var f = Number(document.getElementById("input6").value);
+                   alert( e * f );}
+function keisan4(){var g = Number(document.getElementById("input7").value);
+                   var h = Number(document.getElementById("input8").value);
+                   alert( g / h );}
