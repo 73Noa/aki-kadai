766497b9f20d72a76e28f18a4467523abd4a05af
73Noa <167832828+73Noa@users.noreply.github.com>
2025-01-13 00:09:38 +0900

    Update bombom.js

--- bombom.js
+++ bombom.js
@@ -1,2 +1,3 @@
-function bombom(){document.getElementById('bakudan').src='img/bombom2.jpg';
+function bombom(){document.getElementById('bakudan').src='img/bombo1.jpg';
                  setTimeout(3000);}
+function bombom2(){document.getElementById('bakudan').src='img/bombom2.jpg';}
