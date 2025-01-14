4f46573957d08e3edfaf561edba671bb56ddc199
73Noa <167832828+73Noa@users.noreply.github.com>
2025-01-14 17:12:32 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,13 +1,13 @@
 function syouten4(){document.getElementById('goburin').src='img/gekiha.jpeg';}
-function zangeki(){document.getElementById('goburin').src='img/zangeki.jpg';
+function zangeki0(){document.getElementById('goburin').src='img/zangeki.jpg';
                   setTimeout("syouten4()",2000);}
-function mahou(){document.getElementById('goburin').src='img/honou.jpeg';
+function mahou0(){document.getElementById('goburin').src='img/honou.jpeg';
                   setTimeout("syouten4()",2000);}
 
-function syouten4(){document.getElementById('doragon').src='img/gekiha.jpeg';}
-function zangeki(){document.getElementById('doragon').src='img/zangeki.jpg';
-                  setTimeout("syouten4()",2000);}
-function mahou1(){document.getElementById('doragon').src='img/honou.jpeg';
+function syouten40(){document.getElementById('doragon').src='img/gekiha.jpeg';}
+function zangeki00(){document.getElementById('doragon').src='img/zangeki.jpg';
+                  setTimeout("syouten40()",2000);}
+function mahou00(){document.getElementById('doragon').src='img/honou.jpeg';
                   setTimeout("miss()",2000);}
 function miss(){document.getElementById('doragon').src='img/doragon.jpg';
                alert("敵には効かなかった...");}
