175c9de1b0aebfa3b529386a8d916815c83aa744
73Noa <167832828+73Noa@users.noreply.github.com>
2025-01-14 16:25:53 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
-function zangeki(){document.getElementById('goburin').src='img'}
-function mahou(){}
+function syouten4(){document.getElementById('goburin').src='img/gekiha.jpeg';}
+function zangeki(){document.getElementById('goburin').src='img/zangeki.jpg';
+                  setTimeout("syouten4()",2000);}
+function mahou(){document.getElementById('goburin').src='img/honou.jpeg';
+                  setTimeout("syouten4()",2000);}
 function zangeki1(){}
 function mahou1(){}
 function waza(){}
