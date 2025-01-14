f06b4ebe4a80ac11c8afb1457265c02ae439a078
73Noa <167832828+73Noa@users.noreply.github.com>
2025-01-14 16:08:15 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
-function zangeki(){}
+function zangeki(){document.getElementById()}
 function mahou(){}
 function zangeki1(){}
 function mahou1(){}
 function waza(){}
+
+function bombom(){document.getElementById('bakudan').src='img/bom.jpg';
+                 setTimeout("bombom25()",3000);}
